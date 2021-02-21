4f8f4d6641e9a106cb90fddf21995144be94dd81
lycho33 <cho.lyds@gmail.com>
2021-02-20 21:28:57 -0500

    commit tree a0a5d84ffca3610b4013c6d854eea108460d6f4b

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,5 +1,5 @@
 require 'net/http'
-require 'open-uri'
+require 'open-uri' #URI library
 require 'json'
  
 class GetPrograms
@@ -8,11 +8,21 @@ class GetPrograms
 
   def get_programs
     uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri) #NET::HTTP is a Ruby library that allows your program or application to send HTTP requests.
     response.body
   end
 
+  def program_school # returns a list of the schools or organizations that are running our after school programs.
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]  
+      end
+    end 
+
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
+# programs = GetPrograms.new.get_programs
+# puts programs
